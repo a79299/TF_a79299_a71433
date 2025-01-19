38c4604ce15e7bc8025de6de334fc666faa04da4
Diogo Rodrigues <a79299@ualg.pt>
2025-01-19 19:53:41 +0000

    configuração do yolo, mediapipe (mãoes, cara)

--- main.py
+++ main.py
@@ -7,3 +7,21 @@ import cv2
 import logging
 from ultralytics import YOLO
 import pygame
+
+# Inicializar o mixer de som do Pygame
+pygame.mixer.init()
+
+json_data = []
+
+# configuração e carregamento do modelo YOLO11 para detecção de objetos
+logging.getLogger("ultralytics").setLevel(logging.ERROR)
+model = YOLO("models/yolo11s.pt")
+
+# Configurar MediaPipe para detecção de mãos
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7)
+
+# Configurar MediaPipe para detecção de cara
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.7, min_tracking_confidence=0.7)
